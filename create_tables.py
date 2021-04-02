b161f0dbff62f50db92fd866d8fc1f5ff7dcedb0
jpbacher <bacherjp@gmail.com>
2021-04-02 12:38:38 -0400

    added hr_staging_table

---
+++ create_tables.py
@@ -0,0 +1,21 @@
+# Create staging table for the current data
+hr_staging_table = ("""
+    CREATE TABLE IF NOT EXISTS hr_stage(
+    emp_ID varchar(8),
+    emp_name varchar(50),
+    email varchar(100),
+    hire_dt date,
+    job_title varchar(100),
+    salary int,
+    department_nm varchar(50),
+    manager varchar(50),
+    start_dt date,
+    end_dt date,
+    location varchar(50),
+    address varchar(50),
+    city varchar(50),
+    state varchar(2),
+    education_lvl varchar(50));
+    """)
+
+
