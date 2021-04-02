4c2f3f9b3729e92ca2c43d3cf7f8973cb85d8538
jpbacher <bacherjp@gmail.com>
2021-04-02 12:59:51 -0400

    added department_table

--- create_tables.py
+++ create_tables.py
@@ -1,28 +1,37 @@
 # Create staging table for the current data
 hr_staging_table_create = ("""
     CREATE TABLE IF NOT EXISTS hr_stage(
-        emp_ID varchar(8),
-        emp_name varchar(50),
-        email varchar(100),
-        hire_dt date,
-        job_title varchar(100),
-        salary int,
-        department_nm varchar(50),
-        manager varchar(50),
-        start_dt date,
-        end_dt date,
-        location varchar(50),
-        address varchar(50),
-        city varchar(50),
-        state varchar(2),
-        education_lvl varchar(50)
+        emp_ID VARCHAR(8),
+        emp_name VARCHAR(50),
+        email VARCHAR(100),
+        hire_dt DATE,
+        job_title VARCHAR(100),
+        salary INT,
+        department_nm VARCHAR(50),
+        manager VARCHAR(50),
+        start_dt DATE,
+        end_dt DATE,
+        location VARCHAR(50),
+        address VARCHAR(50),
+        city VARCHAR(50),
+        state vVARCHAR(2),
+        education_lvl VARCHAR(50)
     );
 """)
 
 
+# create final database tables
 manager_table_create = ("""
     CREATE TABLE IF NOT EXISTS manager(
         manager_id SERIAL PRIMARY KEY,
         manager VARCHAR(50)
+    );
+""")
+
+
+department_table_create = ("""
+    CREATE TABLE IF NOT EXISTS department(
+        department_id SERIAL PRIMARY KEY,
+        department_nm VARCHAR(50)
     );
 """)
