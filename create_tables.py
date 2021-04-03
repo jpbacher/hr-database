e24a29cc8ad720323740de99646adb7db29ee21a
jpbacher <bacherjp@gmail.com>
2021-04-03 12:18:27 -0400

    capitalized table names

--- create_tables.py
+++ create_tables.py
@@ -1,6 +1,6 @@
 # Create staging table for the current data
 hr_staging_table_create = ("""
-    CREATE TABLE IF NOT EXISTS hr_stage(
+    CREATE TABLE IF NOT EXISTS HR_Stage(
         emp_ID VARCHAR(8),
         emp_name VARCHAR(50),
         email VARCHAR(100),
@@ -21,7 +21,7 @@ hr_staging_table_create = ("""
 
 # create final database tables
 manager_table_create = ("""
-    CREATE TABLE IF NOT EXISTS manager(
+    CREATE TABLE IF NOT EXISTS Manager(
         manager_id SERIAL PRIMARY KEY,
         manager VARCHAR(50)
     );
@@ -29,7 +29,7 @@ manager_table_create = ("""
 
 
 department_table_create = ("""
-    CREATE TABLE IF NOT EXISTS department(
+    CREATE TABLE IF NOT EXISTS Department(
         department_id SERIAL PRIMARY KEY,
         department_nm VARCHAR(50)
     );
@@ -37,7 +37,7 @@ department_table_create = ("""
 
 
 education_table_create = ("""
-    CREATE TABLE IF NOT EXISTS education(
+    CREATE TABLE IF NOT EXISTS Education(
         education_lvl_id SERIAL PRIMARY KEY,
         education_lvl VARCHAR(50)
     );
@@ -45,7 +45,7 @@ education_table_create = ("""
 
 
 job_table_create = ("""
-    CREATE TABLE IF NOT EXISTS job(
+    CREATE TABLE IF NOT EXISTS Job(
         job_title_id SERIAL PRIMARY KEY,
         job_title VARCHAR(100)
     );
@@ -53,7 +53,7 @@ job_table_create = ("""
 
 
 employee_table_create = ("""
-    CREATE TABLE IF NOT EXISTS employee(
+    CREATE TABLE IF NOT EXISTS Employee(
         emp_id SERIAL PRIMARY KEY,
         emp_name VARCHAR(100),
         email VARCHAR(100),
@@ -63,7 +63,7 @@ employee_table_create = ("""
 
 
 location_table_create = ("""
-    CREATE TABLE IF NOT EXISTS location
+    CREATE TABLE IF NOT EXISTS Location
         location_id SERIAL PRIMARY KEY,
         location VARCHAR(50)
     );
@@ -71,7 +71,7 @@ location_table_create = ("""
 
 
 address_table_create = ("""
-    CREATE TABLE IF NOT EXISTS address(
+    CREATE TABLE IF NOT EXISTS Address(
         address_id SERIAL PRIMARY KEY,
         location_id INT REFERENCES location(location_id),
         city VARCHAR(2),
@@ -81,7 +81,7 @@ address_table_create = ("""
 
 
 employment_history_table_create = ("""
-    CREATE TABLE IF NOT EXISTS employment_history(
+    CREATE TABLE IF NOT EXISTS Employment_History(
         emp_id INT REFERENCES employee(emp_id),
         job_title_id INT REFERENCES job(job_title_id),
         education_lvl_id INT REFERENCES education(education_lvl_id),
@@ -94,6 +94,16 @@ employment_history_table_create = ("""
         PRIMARY KEY (emp_id, job_title_id)
     );
 """)
+
+
+manager_table_insert = ("""
+    INSERT INTO Manager (manager)
+        SELECT DISTINCT(manager)
+        FROM HR_Stage
+""")
+
+
+
 
 
 # query list
