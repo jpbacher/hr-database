62eaeef2eea0f21524c2bf3b6ab9c6e67ce17b49
jpbacher <bacherjp@gmail.com>
2021-04-03 08:32:54 -0400

    add variables

--- load_database.py
+++ load_database.py
@@ -1,24 +1,34 @@
 from psycopg2 import connect, sql
+import configparser
+
 from create_tables import create_table_queries
+import project
 
 
-def create_database(db_name):
+config = configparser.ConfigParser()
+config.read_file(open('db.cfg'))
+USER = config.get('USERNAME')
+HOST = config.get('HOST')
+DB_NAME = config.get('DB_NAME')
+
+
+def create_database():
     """
     Creates and connects to the HR database; returns the connection and cursor to HR database
     """
 
     # connect to the default database
-    conn = connect("host=127.0.0.1 dbname=postgres user=joshbacher")
+    conn = connect(f'host={HOST} dbname=postgres user={USER}')
     curr = conn.cursor()
 
     # create database
-    curr.execute(sql.SQL(f'CREATE DATABASE {db_name};'))
+    curr.execute(sql.SQL(f'CREATE DATABASE {DB_NAME};'))
 
     # close connection to default
     curr.close()
 
     # connect to newly created database
-    conn = connect("host=127.0.0.1 dbname=db_name user=joshbacher")
+    conn = connect(f'host={HOST} dbname={DB_NAME} user={USER}')
     curr = conn.cursor()
 
     return curr, conn
@@ -30,15 +40,18 @@ def create_tables(curr, conn):
     '''
 
     for query in create_table_queries:
+        print(f'Running {query}...')
         curr.execute(query)
         conn.commit()
 
 
-def load_stage_table(curr, conn, csv_location):
-    conn = connect("host=127.0.0.1 dbname=db_name user=joshbacher")
+def load_stage_table(curr, conn, filename):
+    conn = connect(f'host={HOST} dbname={DB_NAME} user={USER}')
     curr = conn.cursor()
 
-    curr.execute(sql.SQL(f'COPY hr_stage FROM {csv_location} DELIMITER "," CSV HEADER;'))
+    curr.execute(sql.SQL(f'COPY hr_stage FROM {project.DATA_DIR/filename} DELIMITER "," CSV HEADER;'))
+
+
 
 
 
