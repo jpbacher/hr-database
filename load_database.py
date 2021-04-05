53fe616b90576717080abfa625e3c13e31f882c7
jpbacher <bacherjp@gmail.com>
2021-04-05 08:32:11 -0400

    fixed loading stage sql statement

--- load_database.py
+++ load_database.py
@@ -7,25 +7,37 @@ from queries import insert_table_queries
 
 config = configparser.ConfigParser()
 config.read_file(open('db.cfg'))
-USER = config.get('USERNAME')
-HOST = config.get('HOST')
-DB_NAME = config.get('DB_NAME')
+USER = config.get('DATABASE', 'USERNAME')
+HOST = config.get('DATABASE', 'HOST')
+DB_NAME = config.get('DATABASE', 'DB_NAME')
 
 
-def load_data_into_tables(curr, conn, filename):
+def load_data_into_tables(curr, conn):
     """
     Load the given csv file into the staging table, and transfer records to appropriate tables.
     """
 
-    conn = connect(f'host={HOST} dbname={DB_NAME} user={USER}')
-    curr = conn.cursor()
-
     # load staging table
-    curr.execute(sql.SQL(f'COPY hr_stage FROM {project.DATA_DIR/filename} DELIMITER "," CSV HEADER;'))
+    curr.execute(sql.SQL(f"COPY HR_Stage "
+                         f"FROM '{project.STAGING_DATA_PATH}' "
+                         f"DELIMITER ',' "
+                         f"CSV HEADER;"))
     conn.commit()
 
-    # load all tables of the database
+    # load records of all the database tables
     for query in insert_table_queries:
         print(f'Running {query}...')
         curr.execute(query)
         conn.commit()
+
+
+def main():
+
+    conn = connect(f'host={HOST} dbname={DB_NAME} user={USER}')
+    curr = conn.cursor()
+
+    load_data_into_tables(curr, conn)
+
+
+if __name__ == "__main__":
+    main()
